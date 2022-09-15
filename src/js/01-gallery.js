802d6e1d8927201d2bb56ae4a5b6f8451bcd2341
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-15 14:37:58 +0300

    Commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,46 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
-const galleryItemsList = document.querySelector('.gallery');
-
-const markUp = onCreatePicture(galleryItems);
-
-galleryItemsList.insertAdjacentHTML('beforeend', markUp);
-function onCreatePicture(items) {
-    return items.map(item => {
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const galleryItemsEl = document.querySelector('.gallery');
+const markUp = onCreateItem(galleryItems);
+galleryItemsEl.insertAdjacentHTML('beforeend', markUp);
+function onCreateItem(items) {
+    return galleryItems.map(({ preview, original, description }) => {
         return `
-       <div class="gallery__item">
-  <a class="gallery__link" href="${item.preview}"
-  data-lightbox = "lbox">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</div>`}).join('')
-}
-
-galleryItemsList.addEventListener('click', e => {
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"
+  title = "${description}" />
+</a>`}).join(''); }
+function onImageClick(e) {
     e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
-		return
-	}
-    const selectedImage = e.target.getAttribute('data-source')
-    const instance = basicLightbox.create(`<img src="${selectedImage}" width="800" height="600">`)
-    instance.show()
-        galleryItemsList.addEventListener('keydown', e => {
-		if (e.key === 'Escape') {
-			instance.close()
-		}
-	})
-})
-
-new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
+      if (e.target.nodeName !== 'IMG') {
+        return
+    }
+}
+new SimpleLightbox('.gallery a', {  captionDelay: 250})
