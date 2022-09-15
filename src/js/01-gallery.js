a8363bc0fa90040be26bf7ecadb3ee69a3473080
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-15 08:38:38 +0300

    +++

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+const galleryItemsList = document.querySelector('.gallery');
+
+const markUp = onCreatePicture(galleryItems);
+
+galleryItemsList.insertAdjacentHTML('beforeend', markUp);
+function onCreatePicture(items) {
+    return items.map(item => {
+        return `
+       <div class="gallery__item">
+  <a class="gallery__link" href="${item.preview}"
+  data-lightbox = "lbox">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`}).join('')
+}
+
+galleryItemsList.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+    const selectedImage = e.target.getAttribute('data-source')
+    const instance = basicLightbox.create(`<img src="${selectedImage}" width="800" height="600">`)
+    instance.show()
+        galleryItemsList.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
 
+    import Player from "@vimeo/player";
+    import Throttle from 'lodash.throttle';
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const timeKey = 'videoplayer-current time'
+
+    function durationSaveToStorage({ seconds }) {
+         localStorage.setItem(timeKey, seconds)
+    }
+window.addEventListener('load', 'newStart');
+player.on('timeupdate', durationSaveToStorage);
+
+function newStart() {
+    if (!localStorage.getItem(timeKey)) {
+        return;
+    }
+}
+const currentVideoTime = localStorage.getItem(timeKey);
+
+
+player
+.setCurrentTime(currentVideoTime)
+    .then(() => {
+        player.play();
+    })
+    .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                breack;
+            default: break;
+    }
+})
+
+
+  
+    
+    
+
