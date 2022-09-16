be330243b7e0c4a408200f7c7c40ff53c2186c7c
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-16 12:42:13 +0300

    +++

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,9 +2,22 @@ import Throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const email = form.querySelector('[name="email"]');
 const message = form.querySelector('[name="message"]');
+// ключ, за яким зберігаються дані в локал-сторедж
 const localKey = 'feedback-form-state';
 form.addEventListener('input', Throttle(storageFormData, 500));
 form.addEventListener('submit', onFormSubmit);
+window.addEventListener('load', checkStorage);
+function checkStorage() {
+  if (!localStorage.getItem(localKey)) return;
+  const formValue = JSON.parse(localStorage.getItem(localKey));
+  console.dir(formValue);
+  //  в значення полів форми присвоїли дані з локал стореджа
+  for (const key in formValue) {
+    form.elements[key].value = formValue[key];
+  }
+  //   email.value = formValue.email;
+  //   message.value = formValue.message;
+}
 function onFormSubmit(event) {
   event.preventDefault();
   // const { email, message } = event.currentTarget.elements;
@@ -18,12 +31,13 @@ function onFormSubmit(event) {
 }
 function storageFormData(event) {
   const formValue = { email: '', message: '' };
+  //   якщо є дані в локал сторедж, копиюємо дані в поля форми через обжект ассайн
   if (localStorage.getItem(localKey)) {
     Object.assign(formValue, JSON.parse(localStorage.getItem(localKey)));
   }
-  //   console.log('name input:', event.target.name);
-  //   console.log('value input:', event.target.value);
-  formValue[event.target.name] = event.target.value;
+  //  записуємо значення яке ввели в інпут, що покласти в локальне сховище
+  formValue[event.target.name] = event.target.value.trim();
   //   console.log(formValue);
-  localStorage.setItem(localKey, JSON.stringify(formValue))
+  // кладемо в локальне сховище те що зберегли в formValue ( в інпутах)
+  localStorage.setItem(localKey, JSON.stringify(formValue));
 }
